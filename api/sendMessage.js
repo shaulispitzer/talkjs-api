1c115e047294e099078be01af826c8e078fdf6f3
Shauli Spitzer <120910625+shaulispitzer@users.noreply.github.com>
2025-05-25 21:58:14 +0300

    Update sendMessage.js

--- api/sendMessage.js
+++ api/sendMessage.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
       iss: process.env.TALKJS_APP_ID,
     },
     process.env.TALKJS_SECRET_KEY,
-    { expiresIn: '30s' }
+    { expiresIn: '55s' }
   );
 
   res.status(200).json({ token });
